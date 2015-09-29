143c3daf7c92b6797a334b2888572802b8305464
renangiuliani <renan.giuliani@gmail.com>
2015-09-28 21:26:05 -0300

    tentando fazer search

--- TestePls/app/src/main/java/com/example/renan/testepls/ListRecipeActivity.java
+++ TestePls/app/src/main/java/com/example/renan/testepls/ListRecipeActivity.java
@@ -1,16 +1,23 @@
 package com.example.renan.testepls;
 
 import android.content.Intent;
+import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.renan.testepls.adapter.ListRecipeAdapter;
 import com.example.renan.testepls.entities.Recipe;
@@ -24,7 +31,7 @@ public class ListRecipeActivity extends AppCompatActivity {
 
     private String recipeType;
     private FloatingActionButton fbAddRecipe;
-    private TextView tvTbName;
+    private TextView tvName;
     private EditText etSearch;
     private ListRecipeAdapter listRecipeAdapter;
     private RecyclerView recyclerView;
@@ -45,7 +52,7 @@ public class ListRecipeActivity extends AppCompatActivity {
         bindElements();
 
         if(recipeType != null){
-            tvTbName.setText(recipeType);
+            tvName.setText(recipeType);
         }
 
     }
@@ -68,7 +75,7 @@ public class ListRecipeActivity extends AppCompatActivity {
 
         ivBack = (ImageView) findViewById(R.id.iv_back);
         ivImage = (ImageView) findViewById(R.id.iv_image);
-        tvTbName = (TextView) findViewById(R.id.tv_tb_name);
+        tvName = (TextView) findViewById(R.id.tv_name);
         etSearch = (EditText) findViewById(R.id.et_search);
         ivSearch = (ImageView) findViewById(R.id.iv_search);
 
@@ -91,6 +98,48 @@ public class ListRecipeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ivSearch.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                if(tvName.getVisibility() == View.VISIBLE) {
+                    ivImage.setVisibility(View.INVISIBLE);
+                    tvName.setVisibility(View.INVISIBLE);
+                    etSearch.setVisibility(View.VISIBLE);
+                    etSearch.requestFocus();
+                }else{
+                    Toast.makeText(ListRecipeActivity.this, "Pesquisando", Toast.LENGTH_SHORT).show();
+                    ivImage.setVisibility(View.VISIBLE);
+                    tvName.setVisibility(View.VISIBLE);
+                    etSearch.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tvName.getVisibility() == View.INVISIBLE) {
+                    ivImage.setVisibility(View.VISIBLE);
+                    tvName.setVisibility(View.VISIBLE);
+                    etSearch.setVisibility(View.INVISIBLE);
+                } else {
+                    Toast.makeText(ListRecipeActivity.this, "Voltar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        etSearch.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                return true;
+            }
+        });
+
+
 
     }
 }
